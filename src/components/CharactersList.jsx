73a7e18975084105aa9129d59a72b6386d2fcf32
Lennin Ibarra <lgibarra@indracompany.com>
2024-01-18 11:07:28 -0500

    refactor: se modifica el llamado a la pagina siguiente

--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -3,21 +3,37 @@ import useCharacter from "../hooks/useCharacter";
 import PropTypes from "prop-types";
 
 function NavPage(props) {
+  const { limitPage } = useCharacter();
+  const params = new URLSearchParams(window.location.search);
+  let page = parseInt(params.get("page"), 10);
+  const nextPage = page < limitPage ? page + 1 : 1;
+
   return (
     <div className="d-flex justify-content-between align-items-center">
       <p>Pagina: {props.page} </p>
       <button
         className="btn btn-primary btn-sm"
-        onClick={() => props.setPage(props.page + 1)}
+        onClick={() => {
+          //   props.page < limitPage
+          //     ? props.setPage(props.page + 1)
+          //     : props.setPage(1);
+
+          const url = new URL(location);
+          url.searchParams.set("page", nextPage);
+          history.pushState({}, "", url);
+
+          props.setPage(nextPage);
+        }}
       >
-        Siguinte: {props.page + 1}
+        Siguiente: {props.page + 1}
       </button>
+      <a href={`/?page=${nextPage}`}>Siguiente</a>
     </div>
   );
 }
 
 function CharactersList() {
-  const { characters, loading, page, setPage } = useCharacter();
+  const { characters, loading, page, setPage, error } = useCharacter();
 
   console.log({ page });
 
@@ -26,6 +42,7 @@ function CharactersList() {
       <NavPage page={page} setPage={setPage} />
       <div className="row">
         {loading && <h1>Loading...</h1>}
+        {error && <h1>{error}</h1>}
         {characters.map((character) => {
           return (
             <div className="col-md-4" key={character.id}>
@@ -33,14 +50,16 @@ function CharactersList() {
             </div>
           );
         })}
+        <NavPage page={page} setPage={setPage} />
       </div>
     </div>
   );
 }
 
 CharactersList.propTypes = {
-  page: PropTypes.string,
+  page: PropTypes.number,
   setPage: PropTypes.func,
+  limitPage: PropTypes.number,
 };
 
 export default CharactersList;
--- src/hooks/useCharacter.js
+++ src/hooks/useCharacter.js
@@ -1,24 +1,46 @@
 import { useEffect, useState } from "react";
 
 function useCharacter() {
+  const params = new URLSearchParams(window.location.search);
+  let historyPage = parseInt(params.get("page"), 10);
   const [loading, setLoading] = useState(true);
   const [characters, setCharacters] = useState([]);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(historyPage);
+  const [limitPage, setLimitPage] = useState();
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch(
-        `https://rickandmortyapi.com/api/character?page=${page}`
-      );
-      const data = await response.json();
-      setLoading(false);
-      setCharacters(data.results);
+      try {
+        const response = await fetch(
+          `https://rickandmortyapi.com/api/character?page=${page}`
+        );
+        setLoading(false);
+        if (!response.ok) {
+          switch (response.status) {
+            case 404:
+              setError("La pagina no existe");
+              break;
+            case 500:
+              setError("Erro desconocido en el servidor");
+              break;
+          }
+
+          return;
+        }
+        const data = await response.json();
+        console.log(data);
+        setCharacters(data.results);
+        setLimitPage(data.info.pages);
+      } catch (e) {
+        console.info(e);
+      }
     }
 
     fetchData();
   }, [page]);
 
-  return { characters, loading, page, setPage };
+  return { characters, loading, page, setPage, limitPage, error };
 }
 
 export default useCharacter;
